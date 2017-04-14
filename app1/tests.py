de9a7cf5288e794c7af42e4f17a517c2b29c0d82
dk05408 <dongkun.sh@superjia.com>
2017-04-14 14:24:22 +0800

    test git

--- app1/tests.py
+++ app1/tests.py
@@ -5,5 +5,17 @@ from django.test import TestCase
 def test(param):
     print("test",param)
 
+def printNum():
+    lower = int(input("输入区间最小值: "))
+    upper = int(input("输入区间最大值: "))
+
+    for num in range(lower,upper+1):
+        if num>1:
+            for i in range(2,num):
+                if(num%i)==0:
+                    break
+            else:print(num);
+
 if __name__=='__main__':
-    test(" haha git")
+    printNum()
+    # test(" haha git")
